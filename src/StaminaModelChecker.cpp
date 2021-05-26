94f7ae10349ebcf6af5632806d9312362943a449
Josh Jeppson <josh.jeppson@aggiemail.usu.edu>
2021-05-26 12:21:45 -0600

    Rewrote StaminaModelChecker::modelCheckStamina()

--- src/StaminaModelChecker.cpp
+++ src/StaminaModelChecker.cpp
@@ -140,6 +140,11 @@ std::unique_ptr<storm::modelchecker::CheckResult> StaminaModelChecker::modelChec
 ) {
     Result* res_min_max[2] = {new Result(), new Result()};
 
+    double reachTh = StaminaOptions::getReachabilityThreshold();
+
+    // Lower and upper bounds on time
+    double lTime, uTime;
+
     // Instantiate and load model generator
     infModelGen = new InfCTMCModelGenerator<double>(modulesFile);
     //super.loadModelGenerator(infModelGen);
@@ -193,8 +198,8 @@ std::unique_ptr<storm::modelchecker::CheckResult> StaminaModelChecker::modelChec
         ((!terminateModelCheck(res_min_max[0]->getResult(), res_min_max[1]->getResult(), StaminaOptions::getProbErrorWindow())) 
         && (numRefineIteration < StaminaOptions::getMaxApproxCount()))) { */
 
-    std::shared_ptr<storm::logic::Formula const> expr = exprProp;
-    std::shared_ptr<storm::logic::Formula const> exprTemp = expr;
+    // std::shared_ptr<storm::logic::Formula const> expr = exprProp;
+    // std::shared_ptr<storm::logic::Formula const> exprTemp = expr;
 
     if(exprTemp->isPathFormula() && (exprTemp->isUntilFormula()) && (!StaminaOptions::getNoPropRefine())) {
         //infModelGen->setPropertyExpression(exprTemp);
@@ -204,17 +209,241 @@ std::unique_ptr<storm::modelchecker::CheckResult> StaminaModelChecker::modelChec
         switchToCombinedCTMC = true;
     }
 
+    if (instanceof</*TODO: ExpressionTemporal*/>(exprProp)) {
+        while (numRefineIteration == 0 || 
+            ((!terminateModelCheck(
+                res_min_max[0].getResult()
+                , res_min_max[0].getResult()
+                , StaminaOptions::getProbErrorWindow()
+            ))
+            && (numRefineIteration < StaminaOptions::getMaxApproxCount()))
+        ) {
+            reachTh = StaminaOptions::getReachabilityThreshold();
+            std::shared_ptr<storm::logic::Formula const> expr = exprProp;
+            if (instanceof</*TODO: ExpressionTemporal*/>(expr)) {
+                /* TODO: Change to cast to the storm equivalent of ExpressionTemporal */
+                std::shared_ptr<storm::logic::Formula const> exprTemp = expr;
+
+                if (exprTemp->isPathFormula() && (exprTemp->getOperator()==/*TODO: ExpressionTemporal*/P_U) && (!StaminaOptions::getNoPropRefine())) {
+                    infModelGen->setPropertyExpression(exprTemp);
+                }
+
+                if (exprTemp->isPathFormula() && (exprTemp->getOperator()==/*TODO: ExpressionTemporal*/P_U)) {
+                    switchToCombinedCTMC = true;
+                }
+
+                if (StaminaOptions::getNoPropRefine()) {
+                    switchToCombinedCTMC = false;
+                }
+
+                /* ============ Approximation Step ============ */
+                std::cout << std::endl;
+                std::cout << "========================================================================" << std::endl;
+                std::cout << "Approximation<" << (numRefineIteration + 1) << "> : kappa = " << reachTh << std::endl;
+                std::cout << "========================================================================" << std::endl;
+                infModelGen->setReachabilityThreshold(reachTh);
+                if (switchToCombinedCTMC) {
+
+                    // Invoke model build explicitly
+                    if (StaminaOptions::getImportModel()) {
+                        // Base filename
+                        std::string filename = StaminaOptions::getImportFileName();
+                        // States file
+                        FILE * sf = nullptr;
+                        std::string statesFile = filename + ".sta";
+                        // Labels file
+                        FILE * lf = nullptr;
+                        std::string labelsFile = filename + ".lab";
+                        // State rewards file
+                        FILE * srf = nullptr;
+                        std::string stateRewardsFile = filename + ".srew";
+                        // Transitions file
+                        FILE * mf = nullptr;
+                        std::string transFile = filename + ".tra";
+                        try {
+                            // TODO: loadModelFromExplicitFiles(). Currently the C++ version doesn't have a superclass
+                        }
+                        catch(const std::exception& e) {
+                            std::cerr << e.what() << std::endl;
+                        }
+                    }
+                    // TODO: buildModel(). The Java version invoked this from the superclass.
+
+                    // Export if necessary
+                    if (StaminaOptions::getExportModel()) {
+                        // Base filename
+                        std::string filename = StaminaOptions::getExportFileName();
+                        // States file
+                        FILE * sf = nullptr;
+                        std::string statesFile = filename + ".sta";
+                        // Labels file
+                        FILE * lf = nullptr;
+                        std::string labelsFile = filename + ".lab";
+                        // State rewards file
+                        FILE * srf = nullptr;
+                        std::string stateRewardsFile = filename + ".srew";
+                        // Transitions file
+                        FILE * mf = nullptr;
+                        std::string transFile = filename + ".tra";
+                        try {
+                            // TODO: Export everything. Currently the C++ version doesn't have a superclass, which the Java version invoked
+                            // to do this part.
+                        }
+                        catch(const std::exception& e) {
+                            std::cerr << e.what() << std::endl;
+                        }
+                    }
+
+                    // Model check operands first for all states.
+                    // TODO: get mcCTMC. Also, find out what size the bitset should be.
+                    std::bitset<8> b2 = // mcCTMC::checkExpression;
+
+                    // Lower bound is set to 0 unless specified (U<=t)
+                    // TODO: get timeExpr. storm::jani::formula has no getLowerBound() method.
+                    auto timeExpr = nullptr; // Also, change this from auto to whatever the storm equivalent of Expression is
+                    
+                    if (timeExpr != nullptr) {
+                        // TODO: get lower bound from exprTemp and mcCTMC
+                        // Throw an error if < 0
+                        STORM_LOG_THROW(timeExpr < 0, storm::exceptions::OutOfRangeException, "Lower bound must be greater than 0");
+                    }
+                    else {
+                        lTime = 0;
+                    }
+
+                    // Default upper bound is -1
+                    // TODO: get upper bound from exprTemp
+                    if (timeExpr != nullptr) {
+                        // TODO: get upper bound from exprTemp and mcCTMC
+                        // Throw error if uTime < 0 or if it is zero and the upper bound is strict
+                        STORM_LOG_THROW(
+                            uTime < 0 // || (uTime == 0 && exprTemp.upperBoundIsStrict())
+                            , storm::exceptions::OutOfRangeException
+                            , "Invalid upper bound in time bounded until formula"
+                        );
+                        STORM_LOG_THROW(uTime < lTime, storm::exceptions::OutOfRangeException, "Upper bound must exceed lower bound");
+                    }
+                    else {
+                        uTime = -1;
+                    }
+
+                    STORM_LOG_THROW(lTime > 0.0, storm::exceptions::OutOfRangeException, "This version of STAMINA only supports causal [0,t] time bounds");
+                    
+                    // Verification
+                    std::cout << "\n\n---------------------------------------------------------------------\n\n";
+                    std::cout << "Verifying " << propName << " ....." << std::endl;
+
+                    timer = std::chrono::system_clock::now();
+
+                    // TODO: run transient analysis
+
+                    double ansMin, ansMax;
+                    ansMin = 0.0;
+
+                    for (int i = 1; i < /* TODO: get total states in explicit build model */; ++i) {
+                        if (b2[i]) {
+                            ansMin += (double) // TODO: get value from transient analysis.
+                        }
+                    }
+                    if (infModelGen->finalModelHasAbsorbing()) {
+                        // TODO: needs to get index of absorbing state
+                        ansMax = ansMin + (double) // TODO: first element from transient analysis.
+                        if (ansMax > 1.0) {
+                            ansMax = 1.0;
+                        }
+                    }
+                    else {
+                        // TODO: get index of absorbing state
+                        ansMax = ansMin;
+                    }
+
+                    timer = std::chrono::system_clock::now() - timer;
+                    // Show elapsed time
+                    std::cout << "Elapsed time for model checking: " << timer / 1000.0 << " seconds." << std::endl;
+
+                    // Set Results
+                    res_min_max[0]->setResultAndExplaination(ansMin, "minimum bound");
+                    res_min_max[1]->setResultAndExplaination(ansMax, "maximum bound");
+
+                    std::cout <<"\nResult:" << res_min_max[0]->getResultsString() << std::endl; // TODO: no implementation exists for Results::getResultsString
+                    std::cout <<"\nResult:" << res_min_max[1]->getResultsString() << std::endl;
+                }
+                else {
+                    // TODO: build model
+                    
+                    // Verification
+                    std::cout << "\n\n---------------------------------------------------------------------\n\n";
+                    std::cout << "Verifying lower bound for " << propMin.getName() << " ....";
+                    // TODO: Invoke model check from superclass (which there is none yet)
+                    std::cout << "\n\n---------------------------------------------------------------------\n\n";
+                    std::cout << "Verifying lower bound for " << propMax.getName() << " ....";
+                    // TODO: Invoke model check from superclass (which there is none yet)
+                }
+
+                // Write results to file
+                try {
+                    std::ofstream outfile("results.txt", std::ofstream::trunc);
+                    outfile << ansMin << "\r\n" << ansMax << "\r\n";
+                    outfile.close();
+                }
+                catch (const std::exception& e) {
+                    std::cerr << e.what() << '\n';
+                }
+
+                // Reduce kappa for refinement
+                double diff = (double) res_min_max[1]->getResult() - (double) res_min_max[0]->getResult();
+                double percentOff = 4 * (diff) / StaminaOptions::getProbErrorWindow();
+                if (percentOff > 100.0) {
+                    percentOff = 100.0;
+                }
+
+                StaminaOptions::setMispredictionFactor(StaminaOptions::getMispredictionFactor() * percentOff);
+
+                // Increment refiinement count
+                if (StaminaOptions::getExportPerimeterStates()) {
+                    try {
+                        std::ofstream outf(StaminaOptions::getExportPerimeterFilename(), , std::ofstream::trunc);
+                        std::vector<std::string> values = infModelGen->getPerimeterStatesVector();
+                        outf << "Iteration: " << numRefineIteration << "\r\n";
+                        for (int i = 0; i < infModelGen->getNumVars(); ++i) {
+                            outf << infModelGen->getVarName(i) << ',';
+                        }
+                        outf << "Current Reachability Probability" << std::endl;
+                        for (int i = 0; i < values.size(); ++i) {
+                            outf << values.at(i) << std::endl;
+                        }
+                        outf.close();
+                    }
+                    catch (const std::exception& e) {
+                        std::cerr << e.what() << '\n';
+                    }
+                    
+                }
+                ifModelGen.clearPerimeterStatesVector();
+                ++numRefineIteration;
+            }
+        }
+    }
+
+    // Print the final results
+
+    std::cout << "\n\n---------------------------------------------------------------------\n\n";
+    std::cout << "Property: " << prop << "\n\n";
+    std::cout << "ProbMin: " << res_min_max[0].getResultsString() << "\n\n";
+    std::cout << "ProbMax: " << res_min_max[1].getResultsString() << "\n\n";
+
+    if (StaminaOptions::getExportTransitionsToFile() != nullptr) {
+        std::cout << "\n\nExporting transition list..." << std::endl;
+        printTransitionActions(infModelGen, StaminaOptions::getExportTransitionsToFile());
+        std::cout << "Export complete." << std::endl;
+    }
 
 
-    //////////////////////////Approximation Step///////////////////////////
-    std::cout << std::endl;
-    std::cout << "========================================================================" << std::endl;
-    std::cout << "Approximation<" << (numRefineIteration + 1) << "> : kappa = " << reachTh << std::endl;
-    std::cout << "========================================================================" << std::endl;
-    //infModelGen->setReachabilityThreshold(reachTh);
+    return res_min_max[0];
 
 
-    auto formulae = storm::api::extractFormulasFromProperties(propertiesVector);
+
+    /* auto formulae = storm::api::extractFormulasFromProperties(propertiesVector);
 
     // Now translate the prism program into a CTMC in the sparse format.
     // Use the formulae to add the correct labelling.
@@ -229,7 +458,7 @@ std::unique_ptr<storm::modelchecker::CheckResult> StaminaModelChecker::modelChec
     auto result = mcCTMC->check(storm::modelchecker::CheckTask<>(*(formulae[0]), true));   // should return pMin? or maaaaybe pMax
     auto quantRes = result->asExplicitQuantitativeCheckResult<double>();
     std::cout << "Quantitative Result: " << quantRes << std::endl;
-    return result;
+    return result; */
     // if(result->isExplicitQuantitativeCheckResult()) {
     //     retu
     // } else {
--- src/StaminaModelChecker.h
+++ src/StaminaModelChecker.h
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <bitset>
 
 #include "StaminaOptions.h"
 #include "InfCTMCModelGenerator.h"
