1145fb985dbbe1be097d762b113543ebb12d870b
Josh Jeppson <joshua.jeppson@usu.edu>
2022-01-10 15:33:18 -0700

    unittest framework wasn't working

--- test/tests.py
+++ test/tests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import unittest
+#import unittest
 import os
 import sys
 
@@ -13,18 +13,30 @@ files = {}
 EXECUTABLE_PATH = "../build/stamina-cplusplus"
 JAVA_EXECUTABLE_PATH = ""
 
-class TestStaminaCpp(unittest.TestCase):
+def getFiles(argv1):
+	global files
+	with open(argv1) as csvFile:
+		for ln in csvFile:
+			model, prop = ln.split(',')
+			print(f"Got model {model} and property {prop}", file=sys.stderr)
+			files[model] = prop
 
+class TestStaminaCpp: #(unittest.TestCase):
 	def test_exitSuccess(self):
-		global files
+		print("Unit Test: Exit Success")
 		for model, prop in files.items():
 			cmd = f"{EXECUTABLE_PATH} {model} {prop}"
+			print(f"Running command:\n\t{cmd}")
 			exitCode = os.system(cmd)
 			if exitCode == 0:
-				print(f"[INFO] Successfull exit", file=sys.stderr)
+				print(f"[TEST::INFO] Successfull exit", file=sys.stderr)
 			else:
-				print(f"[ERROR] Bad exit!\n\tCode: {exitCode}\n\tModel: {model}\n\tProperty: {prop}", file=sys.stderr)
-			self.assertEqual(exitCode, 0)
+				print(f"[TEST::ERROR] Bad exit!\n\tCode: {exitCode}\n\tModel: {model}\n\tProperty: {prop}", file=sys.stderr)
+			#self.assertEqual(exitCode, 0)
 
 if __name__=='__main__':
-	unittest.main()
+	getFiles(sys.argv[1])
+	print("Starting unit tests")
+	#unittest.main()
+	a = TestStaminaCpp()
+	a.test_exitSuccess()
