7aa9aee84b3fab6baa9025cce6123dbf60e2efe9
Josh Jeppson <joshua.jeppson@usu.edu>
2022-01-10 15:17:25 -0700

    Test for exit success

--- test/tests.py
+++ test/tests.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import unittest
+import os
+import sys
 
 '''
 This file automatically tests several things from stamina-cplusplus
@@ -17,4 +19,12 @@ class TestStaminaCpp(unittest.TestCase):
 		global files
 		for model, prop in files.items():
 			cmd = f"{EXECUTABLE_PATH} {model} {prop}"
-			os.system(cmd)
+			exitCode = os.system(cmd)
+			if exitCode == 0:
+				print(f"[INFO] Successfull exit", file=sys.stderr)
+			else:
+				print(f"[ERROR] Bad exit!\n\tCode: {exitCode}\n\tModel: {model}\n\tProperty: {prop}", file=sys.stderr)
+			self.assertEqual(exitCode, 0)
+
+if __name__=='__main__':
+	unittest.main()
