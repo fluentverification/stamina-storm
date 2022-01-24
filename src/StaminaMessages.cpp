ed5fa42482be059c53cd2e4dfb836b3b7d7664ec
Josh Jeppson <joshua.jeppson@usu.edu>
2022-01-24 16:15:29 -0700

    Fixing some bugs x3

--- src/StaminaMessages.cpp
+++ src/StaminaMessages.cpp
@@ -7,37 +7,38 @@
 #include <stdlib.h>
 #include <string_view>
 #include <sstream>
+#include <iostream>
 
 using namespace stamina;
 
 void
 StaminaMessages::errorAndExit(std::string err, uint8_t err_num) {
-	std::err << BOLD(FRED("[ERROR]: "));
-	std::err << BOLD("STAMINA encountered the following error and will now exit: ") << std::endl;
-	std::err << '\t' << err << std::endl;
+	std::cerr << BOLD(FRED("[ERROR]: "));
+	std::cerr << BOLD("STAMINA encountered the following error and will now exit: ") << std::endl;
+	std::cerr << '\t' << err << std::endl;
 	exit(err_num);
 }
 
 void
 StaminaMessages::error(std::string err, uint8_t err_num) {
-	std::err << BOLD(FRED("[ERROR]: "));
-	std::err << BOLD("STAMINA encountered the following (possibly recoverable) error: ") << std::endl;
-	std::err << '\t' << err << std::endl;
+	std::cerr << BOLD(FRED("[ERROR]: "));
+	std::cerr << BOLD("STAMINA encountered the following (possibly recoverable) error: ") << std::endl;
+	std::cerr << '\t' << err << std::endl;
 }
 
 void
 StaminaMessages::warning(std::string warn) {
-	std::err << BOLD(FYEL("[WARNING]: ")) << warn << std::endl;
+	std::cerr << BOLD(FYEL("[WARNING]: ")) << warn << std::endl;
 }
 
 void
 StaminaMessages::info(std::string info) {
-	std::err << BOLD(FBLU("[INFO]: ")) << info << std::endl;
+	std::cerr << BOLD(FBLU("[INFO]: ")) << info << std::endl;
 }
 
 void
 StaminaMessages::good(std::string good) {
-	std::err << BOLD(FGRN("[MESSAGE]: ")) << good << std::endl;
+	std::cerr << BOLD(FGRN("[MESSAGE]: ")) << good << std::endl;
 }
 
 #ifdef DEBUG_PRINTS
