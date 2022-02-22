42ee605999358a5e10f57ed76e291f94f38c3788
Josh Jeppson <joshua.jeppson@usu.edu>
2022-02-22 13:31:16 -0700

    Now it compiles--check if causes memory link

--- src/StaminaModelBuilder.cpp
+++ src/StaminaModelBuilder.cpp
@@ -50,7 +50,7 @@ template <typename ValueType, typename RewardModelType, typename StateType>
 StaminaModelBuilder<ValueType, RewardModelType, StateType>::StaminaModelBuilder(
 	std::shared_ptr<storm::generator::PrismNextStateGenerator<ValueType, StateType>> const& generator
 ) : generator(generator)
-	, stateStorage(storm::storage::sparse::StateStorage(generator->getStateSize()))
+	, stateStorage(*(new storm::storage::sparse::StateStorage<StateType>(generator->getStateSize())))
 	, absorbingWasSetUp(false)
 	, fresh(true)
 {
@@ -519,7 +519,7 @@ stamina::StaminaModelBuilder<ValueType, RewardModelType, StateType>::reset() {
 	stateMap.clear();
 	tMap.clear();
 	piMap.clear();
-	stateStorage = storm::storage::sparse::StateStorage(generator->getStateSize());
+	stateStorage = storm::storage::sparse::StateStorage<StateType>(generator->getStateSize());
 	// stateStorage.stateToId.clear();
 	// stateStorage.deadlockStateIndices.clear();
 	// stateStorage.initialStateIndices.clear();
