42a0f357b3b3d8f39a4011592704c519244da6d2
Josh Jeppson <joshua.jeppson@usu.edu>
2022-05-16 14:51:25 -0600

    Test for state space changes

---
+++ test/scripts/pMinMaxTest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir tmp
+git checkout 7a498e9a53aa8a88d2db61b16583b0e6245ad716
+cmake ../.. && make -j8
+# Kill after 7 minutes
+seq 1 | xargs -II timeout $((7 * 60)) ./stamina-cplusplus $MODEL_FILE $PROPERTIES_FILE 2>>tmp/original.txt
+git checkout 86f6b0786f9bb9390bf989f498291fc5916cceef
+cmake ../.. && make -j8
+# Kill after 7 minutes
+seq 1 | xargs -II timeout $((7 * 60)) ./stamina-cplusplus $MODEL_FILE $PROPERTIES_FILE 2>>tmp/latest.txt
+diff original.txt latest.txt
