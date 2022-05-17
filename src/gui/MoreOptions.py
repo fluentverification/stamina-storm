aea8c2417645c8931fdaf3f1634575fa27d5b730
Josh Jeppson <joshua.jeppson@usu.edu>
2022-05-17 15:33:41 -0600

    Added default to MoreOptions in xstamina

--- src/gui/MoreOptions.py
+++ src/gui/MoreOptions.py
@@ -16,27 +16,93 @@ class MoreOptions(QWidget):
 		, "Kappa Reduction Factor":ITypes.TEXT
 		, "Approximation Factor":ITypes.TEXT
 		, "Probability Window":ITypes.TEXT
-		# , "Property Refinement":ITypes.CHECKBOX
+		, "Maximum Iterations":ITypes.TEXT
+		, "Property Refinement":ITypes.CHECKBOX
 	}
+	OPTIONS_STORM = {
+		"Export Modified Model":ITypes.CHECKBOX
+		, "Export Modified Properties":ITypes.CHECKBOX
+	}
+	OPTIONS_PRISM = {
+		"Rank Transitions":ITypes.CHECKBOX
+		, "Use Power Method":ITypes.CHOICE
+		, "Use Jacobi Method":ITypes.CHOICE
+		, "Use Gauss-Seidel Method":ITypes.CHOICE
+		, "Use Backwards Gauss-Seidel Method":ITypes.CHOICE
+	}
+	# Default values
+	DEFAULT_VALUES = {
+		"Reachability Threshold (Kappa)":1
+		, "Kappa Reduction Factor":1.25
+		, "Approximation Factor":2
+		, "Probability Window":1e-3
+		, "Maximum Iterations":10
+		, "Property Refinement":False
+		, "Export Modified Model":False
+		, "Export Modified Properties":False
+		, "Rank Transitions":False
+		, "Use Power Method":False
+		, "Use Jacobi Method":False
+		, "Use Gauss-Seidel Method":False
+		, "Use Backwards Gauss-Seidel Method":False
+	}
+
 	def __init__(self, parent=None):
 		super(MoreOptions, self).__init__(parent)
 		self.resize(600, 1)
 		self.setWindowTitle("More Options for STAMINA")
 		self.optionWidgets = {}
-		self.mainLayout = QFormLayout()
+		self.mainLayout = QVBoxLayout()
 		self.setLayout(self.mainLayout)
-		self.setUpOptions(self.OPTIONS_GENERAL)
+		self.setUpOptions(self.OPTIONS_GENERAL, "General Options")
+		self.setUpOptions(self.OPTIONS_STORM, "STORM Options")
+		self.setUpOptions(self.OPTIONS_PRISM, "PRISM Options")
+		self.setupFormButtons()
+		self.setInitValues()
 
-	def setUpOptions(self, opts):
+	def setInitValues(self):
+		for description, value in self.DEFAULT_VALUES.items():
+			widget = self.optionWidgets[description]
+			if type(value) == int or type(value) == float or type(value) == str:
+				widget.setText(str(value))
+			elif type(value) == bool:
+				widget.setChecked(value)
+
+
+	def setupFormButtons(self):
+		panel = QFrame()
+		self.defaultsButton = QPushButton("Defaults")
+		self.doneButton = QPushButton("Done")
+		layout = QHBoxLayout()
+		panel.setLayout(layout)
+		layout.addWidget(self.defaultsButton)
+		layout.addWidget(self.doneButton)
+		self.mainLayout.addWidget(panel)
+		self.doneButton.clicked.connect(
+			lambda state : self.hide()
+		)
+		self.defaultsButton.clicked.connect(
+			self.setInitValues
+		)
+
+	def setUpOptions(self, opts, groupName = None):
+		groupBox = QGroupBox(groupName)
+		groupLayout = QFormLayout()
+		groupBox.setLayout(groupLayout)
+		self.mainLayout.addWidget(groupBox)
 		for description, iType in opts.items():
 			if iType == ITypes.TEXT:
 				label = QLabel(f"{description}:")
 				field = QLineEdit()
-				self.mainLayout.addRow(label, field)
+				groupLayout.addRow(label, field)
 				self.optionWidgets[description] = field
-			elif iType == iTypes.CHECKBOX:
-				field = QCheckBox(descriptions)
-				self.mainLayout.addRow(field)
+			elif iType == ITypes.CHECKBOX:
+				field = QCheckBox(description)
+				groupLayout.addRow(field)
+				self.optionWidgets[description] = field
+			elif iType == ITypes.CHOICE:
+				field = QRadioButton(description)
+				groupLayout.addRow(field)
 				self.optionWidgets[description] = field
 
 if __name__=='__main__':
