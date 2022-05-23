424d3a779346e12c1a6dfde78fdd7649802bd6cf
Josh Jeppson <joshua.jeppson@usu.edu>
2022-05-23 17:01:01 -0600

    More stateindexarray

--- src/stamina/util/StateIndexArray.cpp
+++ src/stamina/util/StateIndexArray.cpp
@@ -1,50 +1,71 @@
 #include "StateIndexArray.h"
 
-using namespace stamina;
-using namespace stamina::util;
+namespace stamina {
+	namespace util {
 
-template <typename Statetype, typename ProbabilityStateType>
-StateIndexArray<StateType, ProbabilityStateType>::StateIndexArray(uint8_t blockSizeExponent)
-: blockSize(2 << blockSizeExponent)
-{
-	std::shared_ptr<ProbabilityStateType> * subArray = new std::shared_ptr<ProbabilityStateType>(blockSize);
-	stateArray.insert(subArray);
-}
+		template <typename StateType, typename ProbabilityStateType>
+		StateIndexArray<StateType, ProbabilityStateType>::StateIndexArray(uint8_t blockSizeExponent)
+		: blockSize(2 << blockSizeExponent)
+			, numElements(0)
+		{
+			std::shared_ptr<ProbabilityStateType> * subArray = new std::shared_ptr<ProbabilityStateType>[blockSize];
+			stateArray.insert(subArray);
+		}
 
-template <typename Statetype, typename ProbabilityStateType>
-StateIndexArray<StateType, ProbabilityStateType>::~StateIndexArray() {
-	this->clear();
-}
+		template <typename StateType, typename ProbabilityStateType>
+		StateIndexArray<StateType, ProbabilityStateType>::~StateIndexArray() {
+			this->clear();
+		}
 
-template <typename Statetype, typename ProbabilityStateType>
-void
-StateIndexArray<StateType, ProbabilityStateType>::clear() {
-	for (auto subArray : stateArray) {
-		delete subArray;
+		template <typename StateType, typename ProbabilityStateType>
+		void
+		StateIndexArray<StateType, ProbabilityStateType>::clear() {
+			for (auto subArray : stateArray) {
+				delete subArray;
+			}
+		}
+
+		template <typename StateType, typename ProbabilityStateType>
+		void
+		reserve(uint32_t numToReserve) {
+			this->clear();
+			uint32_t actualNumToReserve = sizeToActualSize(numToReserve);
+			uint16_t arrayIndex = actualNumberToReserve / blockSize;
+			uint32_t subArrayIndex = actualNumberToReserve % blockSize;
+			for (int i = 0; i < arrayIndex; i++) {
+				stateArray.insert(new std::shared_ptr<ProbabilityStateType>[blockSize]);
+			}
+		}
+
+		std::shared_ptr<ProbabilityStateType>
+		StateIndexArray<StateType, ProbabilityStateType>::get(StateType index) {
+			uint16_t arrayIndex = index / blockSize;
+			uint32_t subArrayIndex = index % blockSize;
+			return stateArray[arrayIndex][subArrayIndex];
+		}
+
+		template <typename StateType, typename ProbabilityStateType>
+		void
+		StateIndexArray<StateType, ProbabilityStateType>::put(
+			StateType index
+			, std::shared_ptr<ProbabilityStateType> probabilityState
+		) {
+			numElements++;
+			uint16_t arrayIndex = index / blockSize;
+			uint32_t subArrayIndex = index % blockSize;
+			while (arrayIndex > stateArray.size() - 1) {
+				stateArray.insert(new std::shared_ptr<ProbabilityStateType>[blockSize]);
+			}
+			stateArray[arrayIndex][subArrayIndex] = probabilityState;
+		}
+
+		template <typename StateType, typename ProbabilityStateType>
+		uint32_t
+		StateIndexArray<StateType, ProbabilityStateType>::sizeToActualSize(uint32_t size) {
+			while (size % blockSize != 0) {
+				size++;
+			}
+		}
+
 	}
 }
-
-template <typename Statetype, typename ProbabilityStateType>
-void
-reserve(uint32_t numToReserve) {
-	this->clear();
-	uint32_t actualNumToReserve = sizeToActualSize(numToReserve);
-
-}
-
-std::shared_ptr<ProbabilityStateType>
-StateIndexArray<StateType, ProbabilityStateType>::get(StateType index) {
-	uint16_t arrayIndex = index / stateArray;
-	uint32_t subArrayIndex = index % blockSize;
-	return stateArray[arrayIndex][subArrayIndex];
-}
-
-template <typename Statetype, typename ProbabilityStateType>
-void
-StateIndexArray<StateType, ProbabilityStateType>::put(
-	StateType index
-	, std::shared_ptr<ProbabilityStateType> probabilityState
-) {
-
-}
-
--- src/stamina/util/StateIndexArray.h
+++ src/stamina/util/StateIndexArray.h
@@ -2,6 +2,9 @@
 #define STATEINDEXARRAY_H
 
 #include <vector>
+#include <cstdint>
+#include <memory>
+
 /**
  * Because state indecies are generally assigned in order, it is more efficient to
  * allocate a linear array which guarantees a O(1) lookup time and has an (at worst)
@@ -21,6 +24,7 @@ namespace stamina {
 		protected:
 			uint32_t sizeToActualSize(uint32_t size);
 		private:
+			uint32_t numElements;
 			uint32_t blockSize;
 			std::vector<std::shared_ptr<ProbabilityStateType> * > stateArray;
 		};
