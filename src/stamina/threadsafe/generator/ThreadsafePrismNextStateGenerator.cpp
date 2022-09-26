412bd601a0b2ee43fecb2a35189ab1a516f3d8e2
Josh Jeppson <joshua.jeppson@usu.edu>
2022-09-26 12:09:43 -0600

    These two files are definitions for a Threadsafe drop in for PrismNextStateGenerator

---
+++ src/stamina/threadsafe/generator/ThreadsafePrismNextStateGenerator.cpp
@@ -0,0 +1,38 @@
+#include "ThreadsafePrismNextStateGenerator.h"
+
+namespace stamina {
+namespace threadsafe {
+namespace generator {
+
+template<typename ValueType, typename StateType = uint32_t>
+ThreadsafePrismNextStateGenerator<ValueType, StateType>::ThreadsafePrismNextStateGenerator(
+	storm::prism::Program const& program
+	, storm::generator::NextStateGeneratorOptions const& options = NextStateGeneratorOptions()
+	, std::shared_ptr<ActionMask<ValueType, StateType>> const& = nullptr
+) : PrismNextStateGenerator<ValueType, StateType>(program, options)
+{
+	// intentionally left empty
+}
+
+template<typename ValueType, typename StateType = uint32_t>
+storm::storage::sparse::StateValuations
+ThreadsafePrismNextStateGenerator<ValueType, StateType>::makeObservationValuation() const {
+
+}
+
+template<typename ValueType, typename StateType = uint32_t>
+void
+ThreadsafePrismNextStateGenerator<ValueType, StateType>::addStateValuation(
+	storm::storage::sparse::state_type const& currentStateIndex
+	, storm::storage::sparse::StateValuationsBuilder& valuationsBuilder
+) const {
+
+}
+
+template class ThreadsafePrismNextStateGenerator<double>;
+
+} // namespace generator
+} // namespace threadsafe
+} // namespace stamina
+
+
