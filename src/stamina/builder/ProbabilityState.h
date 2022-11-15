1b98f8069c3dd414f3ae861bf301fcc73569de37
Josh Jeppson <joshua.jeppson@usu.edu>
2022-11-15 16:57:35 -0700

    Revert change

--- src/stamina/builder/ProbabilityState.h
+++ src/stamina/builder/ProbabilityState.h
@@ -104,10 +104,10 @@ namespace stamina {
 		class ProbabilityStatePair {
 		public:
 			ProbabilityState<StateType> * first;
-			std::reference_wrapper<const CompressedState> second;
+			CompressedState second;
 			ProbabilityStatePair(
 				ProbabilityState<StateType> * first
-				, std::reference_wrapper<const CompressedState> second
+				, CompressedState second
 			) : first(first)
 				, second(second)
 			{ /* Intentionally Left Empty */ }
