ef30cf43c51126d1982d4a8b1f5154b6191eac56
Josh Jeppson <joshua.jeppson@usu.edu>
2023-01-11 12:16:26 -0700

    Added skeleton for implementation

--- src/stamina/priority/EventStatePriority.cpp
+++ src/stamina/priority/EventStatePriority.cpp
@@ -3,5 +3,20 @@
 namespace stamina {
 namespace priority {
 
+template <typename StateType>
+float
+EventStatePriority<StateType>::priority(std::shared_ptr<ProbabilityStatePair<StateType>> state) {
+
+}
+
+template <typename StateType>
+bool
+EventStatePriority<StateType>::operatorValue(
+	const std::shared_ptr<ProbabilityStatePair<StateType>> first
+	, const std::shared_ptr<ProbabilityStatePair<StateType>> second
+) {
+
+}
+
 } // namespace priority
 } // namespace stamina
