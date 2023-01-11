849fd53689d6dde6778ba56d7f9b36ac9b623cb4
Josh Jeppson <joshua.jeppson@usu.edu>
2023-01-11 11:03:36 -0700

    State priority

---
+++ src/stamina/priority/StatePriority.h
@@ -0,0 +1,29 @@
+/**
+ * Stamina State Priority Class
+ * Created by Josh Jeppson on 1/11/2023
+ *
+ * Pure virtual abstract class that creates a priority metric on the state passed in
+ * */
+#ifndef STAMINA_PRIORITY_STATEPRIORITY_H
+#define STAMINA_PRIORITY_STATEPRIORITY_H
+
+#include <cstdint>
+
+#include "core/Options.h"
+#include "core/StaminaMessages.h"
+
+#include "builder/ProbabilityState.h"
+#include "builder/StateAndTransitions.h"
+
+template <typename StateType>
+class StatePriority {
+public:
+	virtual static float priority(std::shared_ptr<ProbabilityStatePair<StateType>> state) = 0;
+	virtual bool operatorValue(
+		const std::shared_ptr<ProbabilityStatePair<StateType>> first
+		, const std::shared_ptr<ProbabilityStatePair<StateType>> second
+	) = 0;
+	static StatePriority<StateType> statePriority;
+};
+
+#endif // STAMINA_PRIORITY_STATE_PRIORITY_H
