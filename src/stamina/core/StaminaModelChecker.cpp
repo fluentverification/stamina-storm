9f7d2da04218ac8545ac95ac4bc749a7e3392c3d
Josh Jeppson <joshua.jeppson@usu.edu>
2023-01-24 13:56:09 -0700

    Fixed can't find variables

--- src/stamina/core/StaminaModelChecker.cpp
+++ src/stamina/core/StaminaModelChecker.cpp
@@ -7,6 +7,8 @@
 #include "ANSIColors.h"
 #include "StaminaMessages.h"
 
+#include "core/StateSpaceInformation.h"
+
 #include "storm/builder/BuilderOptions.h"
 #include "storm/storage/expressions/BinaryRelationExpression.h"
 
@@ -106,7 +108,7 @@ StaminaModelChecker::modelCheckProperty(
 	auto options = BuilderOptions(*propMin.getFilter().getFormula());
 	// Create PrismNextStateGenerator. May need to create a NextStateGeneratorOptions for it if default is not working
 	auto generator = std::make_shared<storm::generator::PrismNextStateGenerator<double, uint32_t>>(modulesFile, options);
-
+	StateSpaceInformation::setVariableInformation(generator->getVariableInformation());
 	if (Options::method == STAMINA_METHODS::ITERATIVE_METHOD) {
 		// The reason that this splits into two separate classes is that when calling STAMINA
 		// as a single-threaded application there is less overhead to use just StaminaIterativeModelBuilder
