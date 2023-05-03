3a18e2b0a21c8c6e70e1a092de5d1f82bdd2d1ad
Joshua Jeppson <joshua.jeppson17@gmail.com>
2023-05-03 05:38:37 -0600

    Allow state values to print without pi

--- src/stamina/core/StateSpaceInformation.cpp
+++ src/stamina/core/StateSpaceInformation.cpp
@@ -39,8 +39,10 @@ StateSpaceInformation::stateToString(CompressedState const & state, double pi) {
 	for (auto variable : locationVariables) {
 		StaminaMessages::error("Location Variable printing not implemented yet!");
 	}
-	varString += std::to_string(((int) (pi * 1000000)) / 1000000.0);
-	varString += ")";
+	if (pi != -1.0) {
+		varString += std::to_string(((int) (pi * 1000000)) / 1000000.0);
+		varString += ")";
+	}
 	return varString;
 }
 
