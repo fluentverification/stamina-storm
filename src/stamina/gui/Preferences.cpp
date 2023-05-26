608e1749f5d882c02215094cd0597cae837626be
Josh Jeppson <joshua.jeppson@usu.edu>
2023-05-26 15:41:43 -0600

    Some edits to cmakelists and gui

--- src/stamina/gui/Preferences.cpp
+++ src/stamina/gui/Preferences.cpp
@@ -1,5 +1,7 @@
 #include "Preferences.h"
 
+#include "stamina/StaminaArgParse.h"
+
 namespace stamina {
 namespace gui {
 
@@ -96,15 +98,15 @@ Preferences::getPreferencesFromUI() {
 	PrefInfo::ModelBuilding::perimeterStatesFile = ui->perimPath.text().toStdString();
 	if (ui->reExploring.value()) {
 		// set value
-		PrefInfo::ModelBuilding::truncationMethod =
+		PrefInfo::ModelBuilding::truncationMethod = STAMINA_METHODS::RE_EXPLORING_METHOD;
 	}
 	else if (ui->iterative.value()) {
 		// set value
-		PrefInfo::ModelBuilding::truncationMethod =
+		PrefInfo::ModelBuilding::truncationMethod = STAMINA_METHODS::ITERATIVE_METHOD;
 	}
 	else if (ui->priority.value()) {
 		// set value
-		PrefInfo::ModelBuilding::truncationMethod =
+		PrefInfo::ModelBuilding::truncationMethod = STAMINA_METHODS::PRIORITY_METHOD;
 	}
 	PrefInfo::ModelBuilding::thread = ui->numberThreads.value();
 	// The ModelChecking tab
