e9348661b5452f842f994de90aa2f7948886b765
Josh Jeppson <joshua.jeppson@usu.edu>
2023-07-24 10:26:36 -0600

    Add header to MessageBridge class

--- src/stamina/gui/MessageBridge.h
+++ src/stamina/gui/MessageBridge.h
@@ -1,5 +1,25 @@
 /**
- * A simple bridge class to let StaminaMessages append text to
+ * STAMINA - the [ST]ochasic [A]pproximate [M]odel-checker for [IN]finite-state [A]nalysis
+ * Copyright (C) 2023 Fluent Verification, Utah State University
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ *
+ **/
+
+/**
+ * A simple bridge class to let StaminaMessages append text to the log viewer
+ * in the GUI.
  * */
 #ifndef STAMINA_GUI_MESSAGEBRIDGE_H
 #define STAMINA_GUI_MESSAGEBRIDGE_H
