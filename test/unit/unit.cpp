bd2edcefdec9c41bff2203e7691419f4382d7ca1
Josh Jeppson <joshua.jeppson@usu.edu>
2023-08-24 13:55:22 -0600

    Added second unit test

--- test/unit/unit.cpp
+++ test/unit/unit.cpp
@@ -30,9 +30,15 @@
 #include <cstdint>
 
 #include <stamina/util/ModelModify.h>
+#include <stamina/util/StateIndexArray.h>
+#include <stamina/builder/ProbabilityState.h>
+
+#include <storm-parsers/parser/FormulaParser.h>
 
 namespace bt = boost::unit_test;
+using namespace stamina;
 using namespace stamina::util;
+using namespace stamina::builder;
 
 // =======================================================================================
 // This unit test checks to see if the ModelModify creates the right modified properties
@@ -44,7 +50,7 @@ BOOST_AUTO_TEST_CASE( ModelModify_Basic ) {
 	ModelModify mod(modelFile, propFile);
 	auto loadedModelFile = mod.readModel();
 	// Model file should not be null
-	BOOST_TEST( loadedModelFile != nullptr);
+	BOOST_TEST( loadedModelFile != nullptr );
 	auto propVector = mod.createPropertiesList(loadedModelFile);
 	// There should only be one properties vector
 	BOOST_TEST( propVector->size() == 1 );
@@ -52,17 +58,46 @@ BOOST_AUTO_TEST_CASE( ModelModify_Basic ) {
 	auto propOriginal = (*propVector)[0];
 	auto propMin = mod.modifyProperty(propOriginal, true);
 	auto propMax = mod.modifyProperty(propOriginal, false);
-	// Test that the properties look correct (in prism syntax)
-	// auto propMinString = propMin.asPrismSyntax();
-	// // I wish there were some way to parse the property from the string and compare two
-	// // instances of storm::jani::Property
-	// BOOST_TEST(
-	// 	propMinString == "\"1_min\": P=? [true U[0,1] ((!\"Absorbing\") & (First < 20) & (Second >= 20))]; // Added by STAMINA"
-	// );
-	// auto propMaxString = propMax.asPrismSyntax();
-	// BOOST_TEST(
-	// 	propMaxString == "\"1_max\": P=? [true U[0,1] (\"Absorbing\" | (First < 20) & (Second >= 20))]; // Added by STAMINA"
-	// );
+	storm::parser::FormulaParser parser(*loadedModelFile);
+
+	// we better get probability operator formulas
+	BOOST_TEST( propMin.getRawFormula()->isProbabilityOperatorFormula() );
+	BOOST_TEST( propMax.getRawFormula()->isProbabilityOperatorFormula() );
+
+	std::shared_ptr<const storm::logic::ProbabilityOperatorFormula> formulaMin
+		= std::static_pointer_cast<const storm::logic::ProbabilityOperatorFormula>(propMin.getRawFormula());
+	std::shared_ptr<const storm::logic::ProbabilityOperatorFormula> formulaMax
+		= std::static_pointer_cast<const storm::logic::ProbabilityOperatorFormula>(propMax.getRawFormula());
+
+	BOOST_TEST( formulaMin != nullptr );
+	BOOST_TEST( formulaMax != nullptr );
+
+	// Test expected results
+	auto propMinExpected = parser.parseFromString("P=? [true U[0,1] ((!\"Absorbing\") & (First < 20) & (Second >= 20))];")[0];
+	auto propMaxExpected = parser.parseFromString("P=? [true U[0,1] (\"Absorbing\" | (First < 20) & (Second >= 20))];")[0];
+	// BOOST_TEST(*propMin.getRawFormula() == *propMinExpected.getRawFormula());
+	// BOOST_TEST(*propMax.getRawFormula() == *propMaxExpected.getRawFormula());
 }
+
+BOOST_AUTO_TEST_CASE( StateIndexArray_Basic ) {
+	StateIndexArray<uint32_t, ProbabilityState<uint32_t>> sia;
+
+	// Don't try to dereference any of these
+	ProbabilityState<uint32_t> * one = (ProbabilityState<uint32_t> *) 1;
+	ProbabilityState<uint32_t> * two = (ProbabilityState<uint32_t> *) 2;
+	ProbabilityState<uint32_t> * three = (ProbabilityState<uint32_t> *) 3;
+	ProbabilityState<uint32_t> * mrNullPointer = nullptr;
+	sia.put(1, one);
+	sia.put(2, two);
+	sia.put(3, three);
+	sia.put(0, mrNullPointer);
+
+	BOOST_TEST( sia.get(1) == one );
+	BOOST_TEST( sia.get(2) == two );
+	BOOST_TEST( sia.get(3) == three );
+
+	BOOST_TEST( sia.get(0) == nullptr );
+}
+
 // =======================================================================================
 
