f220158fe98ccfea1fa7203947d3f754afff45f2
Josh Jeppson <joshua.jeppson@usu.edu>
2024-02-02 14:12:48 -0700

    Small changes

--- src/stamina/rare/Subspace.h
+++ src/stamina/rare/Subspace.h
@@ -5,15 +5,32 @@ namespace stamina {
 	namespace rare {
 		class Subspace {
 		public:
-			Subspace(std::vector<Eigen::VectorXd> combinationVectors);
+			static EigenVectorXd zero;
+			Subspace(std::vector<Eigen::VectorXd> combinationVectors, Eigen::VectorXd translation = Subspace::zero);
 
 			uint16_t numSpecies() const;
 			uint16_t dimension() const;
 			const Eigen::MatrixXd & projectionMatrix();
+			virtual double distance(Eigen::VectorXd vec);
 		private:
 			std::vector<Eigen::VectorXd> combinationVectors;
 			Eigen::MatrixXd pMatrix;
+			Eigen::VectorXd & translationVector;
 		};
+
+		/**
+		 * A version of subspace that is orthogonal to one or more
+		 * species dimensions and can short circuit distance calculations.
+		 * Used for the solution space in ragtimer models
+		 * */
+		class OrthSubspace : public Subspace {
+		public:
+			/**
+			 * Overriden constructor taking the species index and desired values for that species in a pair
+			 * */
+			OrthSubspace(std::vector<std::pair<uint16_t, double>> speciesAndValues);
+			OrthSubspace(std::vector<Eigen::VectorXd> combinationVectors, Eigen::VectorXd translation) = delete;
+		}
 	} // namespace rare
 } // namespace stamina
 
